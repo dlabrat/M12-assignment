694ef00e073a1d0a3224ea485f6177a7785d1d0c
dlabrat <107654952+dlabrat@users.noreply.github.com>
2025-01-25 15:39:28 -0800

    initial commit

---
+++ js/oop-practice.js
@@ -0,0 +1,29 @@
+// STEP 1
+class Cat {
+    constructor(name) {
+      this.name = name
+    }
+}
+const Dog = class {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+// STEP 2
+const myCat = new Cat("Mittens");
+const myDog = new Dog("Buddy");
+
+// STEP 3
+
+// STEP 4
+
+// STEP 5
+
+// STEP 6
+
+// STEP 7
+
+// STEP 8
+
+// STEP 9
