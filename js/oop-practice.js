66e9a129560fddb151ac1a387d468627b4ff2bd2
dlabrat <107654952+dlabrat@users.noreply.github.com>
2025-01-25 16:44:03 -0800

    progress

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,4 +1,5 @@
 // STEP 1
+/*
 class Cat {
     constructor(name) {
       this.name = name
@@ -15,10 +16,53 @@ const myCat = new Cat("Mittens");
 const myDog = new Dog("Buddy");
 
 // STEP 3
+class Animal {
+    constructor() {
+
+    }
+    DidIt() {
+        console.log("The Animal has been created")
+    }    
+}
+const myAnimal = new Animal()
+myAnimal.DidIt()
+*/
 
 // STEP 4
+/*
+class Animal {
+    constructor(message) {
+        this.message = message
+    }
+    SayIt() {
+        console.log(this.message)
+    }    
+}
+const myAnimal = new Animal("The animal has been created. ")
+myAnimal.SayIt()
+*/
 
 // STEP 5
+class Animal {
+    constructor(type, breed, color, height, length) {
+      this.type = type
+      this.breed = breed
+      this.color = color
+      this.height= height
+      this.length = length 
+    }
+    displayProps() {
+        console.log(`Animal: 
+            Type: ${this.type} 
+            Breed: ${this.breed}
+            Color: ${this.color}
+            Height: ${this.height} 
+            Length: ${this.length}
+            `)
+    }
+  }
+const myAnimal = new Animal ("Reptile","Corn Snake", "Red", "1 inch", "3 feet")
+myAnimal.displayProps()
 
 // STEP 6
 
